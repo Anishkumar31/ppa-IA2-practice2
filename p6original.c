16e5bad4efe7bd557d931f7a4d3f2e54e9a8427a
Anish kumar <94589270+Anishkumar31@users.noreply.github.com>
2022-04-03 08:54:24 +0000

    p6original

--- p6original.c
+++ p6original.c
@@ -1,33 +1,37 @@
-#include <stdio.h>
-//#include<string.h>
+#include<stdio.h>
+#include <string.h>
 void input_string(char *a)
 {
-  printf("enter any word : ");
-  scanf("%s",a);
+  printf("Enter the string:\n");
+  scanf("%s", a);
 }
-char* str_reverse(char *a)
+
+char *str_reverse(char *str)
 {
-  int m=0;
-  char h;
-  for(int i=0;a[i]!='\0';i++){
-    m++;
+  int length;
+  char t;
+  for (length = 0; str[length] != '\0'; length++);
+  
+  for (int i = 0; i < length; i++,length--)
+  {
+    t = str[i];
+    str[i] = str[length];
+    str[length]=t;
   }
-  for(int i=0;i<m/2;i++){
-    h=a[i];
-    a[i]=a[m-i-1];
-    a[m-i-1]=h;
-  }
-  return a;
+  return str;
 }
-void output(char *ab){
-  printf("\n");
-  printf("Before reverse = %s\n",ab);
-  printf("After reverse = %s\n",str_reverse(ab));
+
+void output(char *a, char *reverse_a)
+{
+  printf("The reverse of %s is %s\n", a, reverse_a);
 }
-int main(){
-  char *a,*r; 
-  input_string(a);
-  r=a;
-  output(r);
+
+int main()
+{
+  char str[10];
+  input_string(str);
+  char t_str[10];
+  strcpy(t_str,str);
+  output(t_str, str);
   return 0;
 }
